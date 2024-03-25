d56762a42545eb47655ee0ee4a9da2be70619bb4
merri4 <kjhwang97@gmail.com>
2024-03-25 18:00:19 +0900

    add : chart

--- public/result.js
+++ public/result.js
@@ -29,3 +29,41 @@ qall_prob.textContent = Math.round(((data["result"][0]["prob"] + data["result"][
 function redirectToMain() {
     window.location.href = '/';
 }
+
+
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['행복', '불행', '그림-긍정', '그림-중립', '그림-부정'],
+        datasets: [{
+        label: 'Logits',
+        data: [q1_prob.textContent, q2_prob.textContent, q3_prob.textContent, q4_prob.textContent, q5_prob.textContent],
+        backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            // 'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            // 'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+        y: {
+            beginAtZero: true
+        }
+        }
+    }
+});
